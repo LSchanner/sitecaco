c80766ddf7fd1d01f68943cfeb39bb301970633b
Lucas Schanner <lsschanner@gmail.com>
2014-12-31 02:50:31 -0200

    permite mudar tamanho da fonte no tinymce

--- static/js/tinymce_setup.js
+++ static/js/tinymce_setup.js
@@ -5,7 +5,7 @@ tinymce.init({
                         "searchreplace visualblocks code fullscreen",
                         "insertdatetime media table contextmenu paste "
                         ],
-                    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+                    toolbar: "insertfile undo redo | styleselect fontsizeselect sizeselect| bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
                     autosave_ask_before_unload: false,
                     image_dimensions: false,
                     extended_valid_elements:"script[language|type|src]",
