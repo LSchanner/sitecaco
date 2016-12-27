ed490b247cb1d6d27ef54c0b04c18c7e34865b3c
henriquefacioli <henrique.facioli@gmail.com>
2016-12-27 18:14:58 -0200

    Criacao do app banco de livros

--- banco_de_livros/views.py
+++ banco_de_livros/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def LivroView(request):
+    busca = request.GET.get('search')
+
+    resultados = ""
+    if(busca):
+        resultados = busca_livros(busca)
+
+    return render(request, 'banco_livros.html', {'resultados':resultados})
+
+#Busca implementada apenas para nome da obra e do autor
+def busca_livros(busca):
+    query_final = Q()
+    palavras = busca.split()
+    for palavra in palavras:
+        query = Q(nome__icontains=palavra) | Q(autor__icontains=palavra)
+        query_final = query & query_final
+
+
+    return Livro.objects.filter(query_final).all()
