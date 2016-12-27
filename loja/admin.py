7b0a53e2afb0a9b23defeb858108c48c9a570d71
henriquefacioli <henrique.facioli@gmail.com>
2016-12-27 18:16:11 -0200

    Criacao do app para apresentacao de produtos

--- loja/admin.py
+++ loja/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from loja.models import Produto
+
+class AdminHtml(admin.ModelAdmin):
+    class Media:
+        js = [
+                '//tinymce.cachefly.net/4.1/tinymce.min.js',
+                '/static/js/tinymce_setup.js',
+                ]
+class AdminProduto(AdminHtml):
+    pass
+
+admin.site.register(Produto,AdminProduto)
--- loja/views.py
+++ loja/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from loja.models import Produto
+
+def ProdutosView(request,id):
+    if(id):
+        id = int(id)
+        produto = Produto.objects.get(id = id)
+        return render(request, 'produto.html', {'produto' : produto})
+
+    produtos = Produto.objects.all()
+    return render(request, 'produtos.html', {'produtos' : produtos})
