b9dc09c80ea74f0b6da5d4c91c3aade0d01890dc
henriquefacioli <henrique.facioli@gmail.com>
2016-12-28 10:24:38 -0200

    Editor de texto de noticias

--- noticias/models.py
+++ noticias/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from ckeditor.fields import RichTextField
+
+
 class Noticia(models.Model):
     user = models.ForeignKey(User, editable = False)
     title = models.CharField(max_length=150)
     time = models.DateTimeField(auto_now_add=True)
-    content = models.TextField()
+    content = RichTextField()
     image = models.ImageField(blank=True)
 
     class Meta:
