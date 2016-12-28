ecd51d6c34d3506028fde196c777322861c6d975
henriquefacioli <henrique.facioli@gmail.com>
2016-12-28 16:50:28 -0200

    Organizacao mais facil do banco de livros

--- banco_de_livros/admin.py
+++ banco_de_livros/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from banco_de_livros.models import Livro
 
 
-admin.site.register(Livro)
+class AdminLivro(admin.ModelAdmin):
+    list_display = ('nome', 'autor', 'edicao', 'disponivel')
+    search_fields = ['nome', 'autor', 'edicao', 'observacao']
+
+admin.site.register(Livro, AdminLivro)
