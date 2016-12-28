03cb9837cbba17706e56ce54ab2898751d7f07e4
henriquefacioli <henrique.facioli@gmail.com>
2016-12-28 14:41:37 -0200

    Mudanca no editor de texto de noticias

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
 from noticias.models import Noticia
 
-
-class AdminHtml(admin.ModelAdmin):
-    class Media:
-        js = [
-                '//tinymce.cachefly.net/4.1/tinymce.min.js',
-                '/static/js/tinymce_setup.js',
-                ]
-
-
-class AdminNoticia(AdminHtml):
+class AdminNoticia(admin.ModelAdmin):
     exclude = ('user',)
     def save_model(self, request, obj, form, change):
         obj.user = request.user
