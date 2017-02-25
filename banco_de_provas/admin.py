083c91df0f273940a517e0c32eefcc17dbb4c81c
henriquefacioli <henrique.facioli@gmail.com>
2017-02-25 17:18:54 -0300

    Corrige problema de aprovar novas provas

--- banco_de_provas/admin.py
+++ banco_de_provas/admin.py
@@ -6,6 +6,7 @@ from banco_de_provas.forms import ProvaForm
 
 class AdminProva(admin.ModelAdmin):
     form = ProvaForm
+    fields = ( 'aprovado', 'materia', 'semestre', 'professor', 'tipo', 'file')
     list_display = ('materia', 'tipo', 'semestre', 'professor', 'aprovado')
     search_fields = ['materia','tipo','semestre','professor']
 
