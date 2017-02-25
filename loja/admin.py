1aa317dd2d5bab059ec4a95652201dfd7d8c5514
henriquefacioli <henrique.facioli@gmail.com>
2017-02-25 11:21:13 -0300

    Usando editor de texto CKEditor na loja

--- loja/admin.py
+++ loja/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 
 from loja.models import Produto
+from loja.forms import formsProduto
 
-class AdminHtml(admin.ModelAdmin):
-    class Media:
-        js = [
-                '//tinymce.cachefly.net/4.1/tinymce.min.js',
-                '/static/js/tinymce_setup.js',
-                ]
-class AdminProduto(AdminHtml):
-    pass
+
+class AdminProduto(OrderedModelAdmin):
+    form = formsProduto
+    list_display = ('name', 'price', 'move_up_down_links')
+    search_fields = ['name', 'price']
 
 admin.site.register(Produto,AdminProduto)
